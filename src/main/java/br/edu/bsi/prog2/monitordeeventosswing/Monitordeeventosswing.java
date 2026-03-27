8647ffde4d176f8c8c5ae4c2e090a61158b85067
luiz1 <luiz1@Lz>
2026-03-27 19:49:45 -0300

    Iniciando projeto

---
+++ src/main/java/br/edu/bsi/prog2/monitordeeventosswing/Monitordeeventosswing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.bsi.prog2.monitordeeventosswing;
+
+/**
+ *
+ * @author luiz1
+ */
+public class Monitordeeventosswing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
